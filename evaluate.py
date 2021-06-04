0776765c2d1f6be99ae9f30b153415372ab55b14
sil320 <64934947+sil320@users.noreply.github.com>
2021-06-04 10:37:34 +0700

    added evaluation

--- evaluate.py
+++ evaluate.py
@@ -11,6 +11,8 @@ data = transform.data_importer_IDS()
 train_data= data.train.samples.values.reshape(data.train.samples.shape[0], data.train.samples.shape[1], 1)
 test_data= data.test.samples.values.reshape(data.test.samples.shape[0], data.test.samples.shape[1], 1)
 
+print(data.test.labels)
+
 IDS=Sequential()
 IDS.add(Conv1D(32, 3, input_shape=train_data.shape[1:]))
 IDS.add(Activation('relu'))
@@ -25,6 +27,33 @@ IDS.load_weights("models/ids/weight.h5")
 
 result=IDS.predict(test_data)
 print(result[0])
+
+TP = 0
+TN = 0
+FP = 0
+FN = 0
+
+for i in range(result.argmax(axis=1)):
+    temp_result = result.argmax(axis=1)[i]
+    temp_test = data.test.labels[i]
+    if temp_result == temp_test:
+        if temp_result == 0:
+            TP = TP + 1
+        else:
+            TN = TN + 1
+    else:
+        if temp_result == 1:
+            FP = FP + 1
+        else:
+            FN = FN + 1
+
+print('Accuracy: ', (TP+TN)/(TP+TN+FP+FN))
+precision = TP/(TP+FP)
+recall = TP/(TP+FN)
+print('Precision: ', precision)
+print('Recall: ', recall)
+print('F1: ', 2*(precision*recall)/(precision+recall))
+
 
 G=torch.load('G_model.pth')
 z = Variable(torch.randn(64,9))
